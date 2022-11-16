5258d05c0df49230c5bb64bd9bc39314cd5d1114
Ayush <ayush17solanki@gmail.com>
2022-11-16 23:41:09 +0530

    Custom User Model

--- VAYU_MAIN/admin.py
+++ VAYU_MAIN/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+from VAYU_MAIN.models import User
+
+
+class UserAdminister(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+
+    model = User
+
+    list_display = ('email', 'fullname', 'is_active',
+                    'is_staff', 'is_superuser', 'last_login',)
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    fieldsets = (
+        (None, {'fields': ('email', 'fullname', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active',
+         'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'fullname', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email', 'fullname')
+    ordering = ('email', 'fullname')
+
+
+admin.site.register(User, UserAdminister)
--- VAYU_MAIN/models.py
+++ VAYU_MAIN/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+from django.utils.translation import gettext as _
+from .managers import UserManager
+
+
+class User(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True)
+    fullname = models.TextField(_("full name"),max_length=50)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('fullname',)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
